8e8a3270b6ffbc4ac37c4a7a4f57df944905b4d0
JacksWebBrand <jbizzell@protonmail.com>
2020-08-21 19:40:30 +0100

    adjusting star params

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 for (var i = 0; i < 100; i++) {
-    var star = '<div class="star" style="animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*$(window).height()/2+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
+    var star = '<div class="star" style="animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*$(window).height()/2.25+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
     $('stars').append(star);
 }
