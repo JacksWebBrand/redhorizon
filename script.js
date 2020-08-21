ea8c0eb0936d1f5a7402f7d66652c226b2d43409
JacksWebBrand <jbizzell@protonmail.com>
2020-08-21 22:23:50 +0100

    hard-coded star values generated by js

--- script.js
+++ script.js
@@ -4,14 +4,4 @@ function setVideoPlaybackRate() {
     landingVideo.playbackRate = 0.5;
 }
 
-function stars () {
-  for (var i = 0; i < 50; i++) {
-    var star = '<div class="star" style="animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*$(window).height()/1.5+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
-    $('stars').append(star);
-    console.log('star');
-}  
-}
-
-// On load
-stars();
 setVideoPlaybackRate();
