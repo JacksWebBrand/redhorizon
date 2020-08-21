02e4a0dd3ef45092a214b1d2d0008fe7d56e2bbc
JacksWebBrand <jbizzell@protonmail.com>
2020-08-21 21:25:11 +0100

    reducing video quality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function setVideoPlaybackRate() {
 }
 
 function stars () {
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 50; i++) {
     var star = '<div class="star" style="animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*$(window).height()/1.5+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
     $('stars').append(star);
     console.log('star');
@@ -13,5 +13,5 @@ function stars () {
 }
 
 // On load
-stars()
-setVideoPlaybackRate()
+stars();
+setVideoPlaybackRate();
