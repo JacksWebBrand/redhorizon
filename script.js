6502e8435f3a79af3d5f29043f5727fb4c21325a
JacksWebBrand <jbizzell@protonmail.com>
2020-08-22 01:31:55 +0100

    down to earth

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const landingVideo = document.getElementById('landing-video');
+const scroll = document.querySelector('.scroll');
 
+// Set landing video playbackrate
 function setVideoPlaybackRate() {
     landingVideo.playbackRate = 0.5;
 }
 
+function downToEarth() {
+    setTimeout(() => {
+        scroll.style.display = 'flex';
+    }, 3000);
+}
+
+
+// On load
 setVideoPlaybackRate();
+downToEarth();
